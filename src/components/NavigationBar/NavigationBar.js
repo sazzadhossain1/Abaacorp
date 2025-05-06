3bcab5e14ed5f0c987d1c16db2e74c4106451ed6
sazzad tomal <sazzadtomal1@gmail.com>
2025-05-06 14:43:23 +0600

    create some components

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavigationBar.css";
+
+const NavigationBar = () => {
+  return (
+    <div>
+      <h1>This is NavigationBar</h1>
+    </div>
+  );
+};
+
+export default NavigationBar;
