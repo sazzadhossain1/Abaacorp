b09e9bf4c304baff3813db65f1e0e0f3b0638ffe
sazzad tomal <sazzadtomal1@gmail.com>
2025-08-11 17:24:30 +0600

    first and seecond section responsive done

--- src/components/SecondSection/SecondSection.js
+++ src/components/SecondSection/SecondSection.js
@@ -1,19 +1,18 @@
 import React from "react";
 import "./SecondSection.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlay, faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 import playerPhoto from "../../accets/secondSectionPhoto/playerPhoto.webp";
 
 const SecondSection = () => {
   return (
-    <div>
+    <div className="secondSection_parent_div">
       <div className="secondSection_text_div">
         <h1>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nemo
           quaerat dolore corrupti alias eum maiores suscipit exercitationem
           molestiae.
         </h1>
-        <FontAwesomeIcon icon={faHouse} />
       </div>
       <div className="player_div">
         <div className="player_img_div">
