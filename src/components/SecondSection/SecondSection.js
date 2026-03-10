c3eb1e2eea51b838d65b3b120a7824e3a9af4629
sazzad tomal <sazzadtomal1@gmail.com>
2026-03-10 18:55:45 +0600

    second section responsive done

--- src/components/SecondSection/SecondSection.js
+++ src/components/SecondSection/SecondSection.js
@@ -1,24 +1,17 @@
 import React from "react";
 import "./SecondSection.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 import playerPhoto from "../../accets/secondSectionPhoto/playerPhoto.webp";
 
 const SecondSection = () => {
   return (
     <div className="secondSection_parent_div">
-      <div className="secondSection_text_div">
-        {/* <h1>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nemo
-          quaerat dolore corrupti alias eum maiores suscipit exercitationem
-          molestiae.
-        </h1> */}
-      </div>
+      <div className="secondSection_text_div"></div>
+
       <div className="player_div">
         <div className="player_img_div">
-          <img className="playerPhoto" src={playerPhoto} alt="" />
-          <div>
-            {/* <FontAwesomeIcon className="faCirclePlay" icon={faCirclePlay} /> */}
+          <img className="playerPhoto" src={playerPhoto} alt="player" />
+
+          <div className="text_overlay">
             <h2 className="top_quality">
               Best Service at a Very Affordable Rate
             </h2>
